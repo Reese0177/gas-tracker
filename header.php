9fbd182bbabe47059df5ecb1154a35756b019e34
Reese0177 <vinnygo@gmail.com>
2022-06-23 16:29:54 -0500

    Fixing db connections

--- header.php
+++ header.php
@@ -30,16 +30,16 @@
         echo "<a id='logout' href='logout.php'>Log Out</a>";
     }
 
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "password";
-    $dbname = "gas-tracker";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("SQL Connection failed: " . $conn->connect_error);
-    }
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"], 1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     ?>
 
 </header>
